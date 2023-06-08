9cfe39f550399ee9c5f49c11942e71beed188c2c
Jasleen8801 <jkaur1_be21@thapar.edu>
2023-06-08 10:50:05 +0530

    student login ho gya

--- frontendFinal/src/pages/student/login.js
+++ frontendFinal/src/pages/student/login.js
@@ -1,9 +1,10 @@
-// https://www.seedprod.com/landing-page-examples/
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 import Cookies from "js-cookie";
+import "../../styles/student/login.css";
+
+import logo from "../../assets/logo.png"
 
 const StudentLogin = () => {
   const [error, setError] = useState("");
@@ -17,7 +18,7 @@ const StudentLogin = () => {
     if (isLoggedIn) {
       navigate("/student/profile");
     }
-  });
+  }, [navigate]);
 
   const handleChange = ({ currentTarget: input }) => {
     setUser({ ...user, [input.name]: input.value });
@@ -50,40 +51,72 @@ const StudentLogin = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="login-box">
-          <h2>Student Login</h2>
-          {error && <div className="alert alert-danger">{error}</div>}
-          <form onSubmit={handleSubmit}>
-            <div className="user-box">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <img src={logo} alt="logo" className="navbar--logo" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/promo">
+                  Promo Page
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="student-login-container">
+        <div className="student-login-box">
+          <h2 className="student-login-title">Student Login</h2>
+          {error && <div className="student-login-alert">{error}</div>}
+          <form className="student-login-form" onSubmit={handleSubmit}>
+            <div className="student-login-user-box">
               <input
                 type="text"
                 name="userName"
                 required
+                className="student-login-input"
                 onChange={handleChange}
                 value={user.userName}
               />
-              <label>Username</label>
+              <label className="student-login-label">Username</label>
             </div>
-            <div className="user-box">
+            <div className="student-login-user-box">
               <input
                 type={showPassword ? "text" : "password"}
                 name="password"
                 required
+                className="student-login-input"
                 onChange={handleChange}
                 value={user.password}
               />
-              <label>Password</label>
+              <label className="student-login-label">Password</label>
               <i
-                className="fa fa-eye password-icon"
+                className="fa fa-eye student-login-password-icon"
                 onClick={handleTogglePassword}
               ></i>
             </div>
-            <button type="submit">
+            <button type="submit" className="student-login-button">
               Login
             </button>
-            <Link to="/student/signup">
-              <button>Signup</button>
+            <Link to="/student/signup" className="student-login-link-button">
+              <button className="student-signup-button">Signup</button>
             </Link>
           </form>
         </div>
